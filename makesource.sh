b79e5029ed1b8559d3d0ab863deac5fe8f67c013
Renato Silva <br.renatosilva@gmail.com>
2015-07-28 11:14:58 -0300

    Update source bundle generation.
    * Download source packages from new location.
    * Build source bundles for each architecture.

--- makesource.sh
+++ makesource.sh
@@ -12,24 +12,25 @@ packages=(gcc-libs::gcc
 
 package_architecture=$(gcc -dumpmachine)
 package_architecture="${package_architecture%%-*}"
+architecture="${package_architecture/x86_64/x64}"
+architecture="${architecture/i686/x86}"
 mingw_package_prefix="mingw-w64-${package_architecture}"
 app_version=$(grep 'define VERSION' winutils.nsi | awk -F'"' '{ printf $2 }')
-zip_file="WinUtils ${app_version} Source.zip"
-mkdir -p libraries
+zip_file="WinUtils ${app_version} ${architecture} Source.zip"
+libraries="libraries/${architecture}"
+mkdir -p "${libraries}"
 
 for package in "${packages[@]}"; do
     binary_package="${mingw_package_prefix}-${package%::*}"
     package_version=$(pacman -Q ${binary_package} | sed 's/.* //')
-    source_prefix="mingw-w64-i686-${package#*::}"
+    source_prefix="${mingw_package_prefix}-${package#*::}"
     source_package="${source_prefix}-${package_version}.src.tar.gz"
-    source_url="https://sourceforge.net/projects/msys2/files/REPOS/MINGW/Sources/${source_package}"
-
-    [[ -s "libraries/${source_package}" ]] && continue || rm -fv libraries/${source_prefix}*src.tar.gz
-    wget --no-verbose --directory-prefix 'libraries' "${source_url}" || exit
+    [[ -s "${libraries}/${source_package}" ]] && continue || rm -fv ${libraries}/${source_prefix}*src.tar.gz
+    wget --no-verbose --directory-prefix "${libraries}" "http://repo.msys2.org/mingw/sources/${source_package}" || exit
 done
 
 echo 'Exporting repository'
 git archive --format=zip --output="${zip_file}" HEAD
 
 echo 'Adding libraries'
-zip -9 -r "${zip_file}" libraries/*.src.tar.gz
+zip -9 -r "${zip_file}" ${libraries}/*.src.tar.gz

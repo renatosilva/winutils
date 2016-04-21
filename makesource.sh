eec48a0c89a28fd7d5ab9d5eb169f641aaeb1580
Renato Silva <br.renatosilva@gmail.com>
2016-04-21 20:31:12 -0300

    Update source bundle generation.

--- makesource.sh
+++ makesource.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # WinUtils Source Bundle Generator
-# Copyright (C) 2015 Renato Silva
+# Copyright (C) 2015, 2016 Renato Silva
 
 # binary[::source]
 packages=(gcc-libs::gcc
@@ -14,19 +14,22 @@ package_architecture=$(gcc -dumpmachine)
 package_architecture="${package_architecture%%-*}"
 architecture="${package_architecture/x86_64/x64}"
 architecture="${architecture/i686/x86}"
-mingw_package_prefix="mingw-w64-${package_architecture}"
 app_version=$(grep 'define VERSION' winutils.nsi | awk -F'"' '{ printf $2 }')
 zip_file="WinUtils ${app_version} ${architecture} Source.zip"
 libraries="libraries/${architecture}"
 mkdir -p "${libraries}"
 
 for package in "${packages[@]}"; do
-    binary_package="${mingw_package_prefix}-${package%::*}"
-    package_version=$(pacman -Q ${binary_package} | sed 's/.* //')
-    source_prefix="${mingw_package_prefix}-${package#*::}"
-    source_package="${source_prefix}-${package_version}.src.tar.gz"
-    [[ -s "${libraries}/${source_package}" ]] && continue || rm -fv ${libraries}/${source_prefix}*src.tar.gz
-    wget --no-verbose --directory-prefix "${libraries}" "http://repo.msys2.org/mingw/sources/${source_package}" || exit
+    binary_name="${package%::*}"
+    source_name="${package#*::}"
+    package_version=$(pacman -Q mingw-w64-${package_architecture}-${binary_name} | sed 's/.* //')
+    source_package_full="mingw-w64-${package_architecture}-${source_name}-${package_version}.src.tar.gz"
+    source_package_simple="mingw-w64-${source_name}-${package_version}.src.tar.gz"
+    test -s "${libraries}/${source_package_full}"   && continue
+    test -s "${libraries}/${source_package_simple}" && continue
+    rm -fv "${libraries}"/mingw-w64-*"${source_name}"*.src.tar.gz
+    wget --no-verbose --directory-prefix "${libraries}" "http://repo.msys2.org/mingw/sources/${source_package_full}"   ||
+    wget --no-verbose --directory-prefix "${libraries}" "http://repo.msys2.org/mingw/sources/${source_package_simple}" || exit
 done
 
 echo 'Exporting repository'
